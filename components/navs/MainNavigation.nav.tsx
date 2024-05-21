eba27a531c444c2d63e06254b16d81ae5236230f
Fandika <fandika.ikhsan@gmail.com>
2024-05-22 03:56:51 +0700

    feat: change routing method to expo route

--- components/navs/MainNavigation.nav.tsx
+++ components/navs/MainNavigation.nav.tsx
@@ -1,46 +1,39 @@
-import { View, StyleSheet, TouchableOpacity } from "react-native"
+import { View, StyleSheet, TouchableOpacity, Pressable } from "react-native"
+import { Link } from "expo-router"
 import { Ionicons } from "@expo/vector-icons"
 import { BlurView } from "expo-blur"
+import { NavigationContainer } from "@react-navigation/native"
 
 export default function MainNavigation({ active }: { active: string }) {
   return (
     <View style={styles.container}>
-      <BlurView intensity={100} style={styles.blur}>
-        <View style={styles.nav}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              window.location.href = "/list"
-            }}
-          >
+      {/* <BlurView intensity={100} style={styles.blur}> */}
+      <View style={styles.nav}>
+        <Link replace href="/list" asChild>
+          <TouchableOpacity style={styles.button}>
             <Ionicons
               name="list"
               size={24}
               color={active === "list" ? "#1D7BE9" : "black"}
             />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.centerButton}
-            onPress={() => {
-              window.location.href = "/"
-            }}
-          >
+        </Link>
+        <Link href="/" asChild>
+          <TouchableOpacity style={styles.centerButton}>
             <Ionicons name="add" size={24} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              window.location.href = "/budget"
-            }}
-          >
+        </Link>
+        <Link href={"/budget"} asChild>
+          <TouchableOpacity style={styles.button}>
             <Ionicons
               name="wallet"
               size={24}
               color={active === "budget" ? "#1D7BE9" : "black"}
             />
           </TouchableOpacity>
-        </View>
-      </BlurView>
+        </Link>
+      </View>
+      {/* </BlurView> */}
     </View>
   )
 }
@@ -61,11 +54,12 @@ const styles = StyleSheet.create({
   nav: {
     flexDirection: "row",
     justifyContent: "space-around",
+    alignItems: "center",
     width: "100%",
     padding: 10,
   },
   button: {
-    padding: 10,
+    padding: 15,
   },
   centerButton: {
     padding: 10,
